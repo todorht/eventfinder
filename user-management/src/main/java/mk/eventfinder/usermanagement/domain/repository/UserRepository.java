4103f55a7475238205c769e5f6b0477dc6310a7d
Hristijan Todorovski <hr.todorovski@gmail.com>
2021-12-06 23:48:50 +0100

    Domashno 2

--- user-management/src/main/java/mk/eventfinder/usermanagement/domain/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package mk.eventfinder.usermanagement.domain.repository;
-
-import Repository.GenericRepository;
-import mk.eventfinder.usermanagement.domain.model.User;
-
-public interface UserRepository extends GenericRepository<User, String> {
-}
